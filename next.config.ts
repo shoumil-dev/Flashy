6ba554885ced122f535c3acb1ac2cbe3b19a6a7d
Shoumil Guha <sguh0003@student.monash.edu>
2025-10-28 20:50:49 +0800

    change default page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/quiz",
+        permanent: true, // use false if you want it temporary during testing
+      },
+    ];
+  },
 };
 
 export default nextConfig;
