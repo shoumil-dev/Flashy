dec09ff2ee0c202b07115bff9ddf4bfb545352f5
Shoumil Guha <sguh0003@student.monash.edu>
2025-11-08 17:09:52 +0800

    remove layout tag from default page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <layout>
       <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <Image
@@ -100,6 +99,5 @@ export default function Home() {
           </a>
         </footer>
       </div>
-    </layout>
   );
 }
